0bc607f9ffc0501caed7209a1654fc7a75edfa28
Maks <maxim.kalashnyk@gmail.com>
2018-11-23 00:21:14 +0200

    js file is changed

--- index.js
+++ index.js
@@ -6,26 +6,42 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+
+let inhabitant = function(species, name, gender, legs, hands, saying){
+	
+	this.species = species;
+	this.name = name;
+	this.gender = gender;
+	this.legs = legs;
+	this.hands = hands;
+	this.saying = saying;
+	
+};
+
+let dog = new inhabitant('dog', 'Sharik', 'male', 4, 0, 'Bow-wow!');
+let cat = new inhabitant('cat', 'Pushok', 'male', 4, 0, 'Meoooow!!!');
+let woman = new inhabitant('woman', 'Elizabeth Taylor', 'female', 2, 2, 'Hello!');
+let man = new inhabitant('man', 'John Doe', 'male', 2, 2, 'Hi!');
+
 
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
+   
+print('<strong>Dog greetings</strong>');
+print('<blockquote>' + dog.saying + '</blockquote>');
+print(' You hear ' + dog.species + ' ' + dog.name + '. He likes playing with ' + cat.name + ' and he also has ' + dog.legs + ' paws.', 'div');
+print('<hr>');
+print('<strong>Cat greetings</strong>');
+print('<blockquote>' + cat.saying + ' ' + cat.saying + '</blockquote>');
+print(' It\'s ' + cat.species + ' ' + cat.name + ' asks for eating this big fish ' + 'and he has ' + cat.legs + ' paws.', 'div', 'div');
+print('<hr>');
+print('<strong>Woman greetings</strong>');
+print('<blockquote>' + woman.saying + '</blockquote>');
+print(' I\'m ' + ' ' + woman.name + ' and I work as a teacher. Also I am a ' + woman.gender + ' ' + 'and I have ' + woman.legs + ' legs and ' + woman.hands + ' hands.', 'div');
+print('<hr>');
+print('<strong>Man greetings</strong>');
+print('<blockquote>' + man.saying + '</blockquote>');
+print(' My name is ' + man.name + ' and I like to travel.' + ' I\'m a 23 years old ' + man.gender + ' ' + 'and I have ' + man.legs + ' legs and ' + man.hands + ' hands.', 'div');
+print('<hr>');
+print('<br>');
 
